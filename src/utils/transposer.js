cbfd20f09cd034789449ecd9ce047426b89d1691
Jacob Krch <jekrch@gmail.com>
2020-12-27 15:47:11 -0600

    chord transposer and tests
    * added utility for transposing chords based on key change

---
+++ src/utils/transposer.js
@@ -0,0 +1,70 @@
+
+// returns the note numbers for the specified chord with the indicated 
+// transposition applied to each note 
+export function shiftChord(originalKeyNumber, newKeyNumber, chordNoteNumbers) {
+        
+    var negativeTransposeModifier = originalKeyNumber - newKeyNumber;
+
+    // determine whether the new chord needs to shift up or down 
+    // and octave in order to stay within the keyboard range 
+    var requiredOctaveModifier = getRequiredOctaveModifier(negativeTransposeModifier, chordNoteNumbers);
+    
+    var newChordNoteNumbers = [];
+
+    for (var i = 0; i < chordNoteNumbers.length; i++) {
+    
+        var noteNumber = chordNoteNumbers[i] - negativeTransposeModifier;
+
+        // if the chord needed to be shifted up or down and octave 
+        // apply that here
+        noteNumber = noteNumber + requiredOctaveModifier;
+        
+        // in the rare case that we're still outside the range 
+        // address that here
+        noteNumber = sanitizeKeyLocation(noteNumber);
+
+        newChordNoteNumbers.push(noteNumber)
+    }
+   	
+    return newChordNoteNumbers;
+}
+
+// if the note goes above or below the keyboard range, move it
+function sanitizeKeyLocation(noteNumber) {
+    if (noteNumber < 0)
+        return noteNumber + 12;
+
+    if (noteNumber > 24)
+        return noteNumber - 12;
+
+    return noteNumber;
+}
+
+// provides a modifier for all transposed notes based on whether 
+// the required transposition would produce any notes that either 
+// go below or above the available keyboard range (24)
+function getRequiredOctaveModifier(negativeTransposeModifier, chordNoteNumbers) {
+
+    var tooLow;
+    var tooHigh;
+    
+    for (var i = 0; i < chordNoteNumbers.length; i++) {
+    
+        var noteNumber = chordNoteNumbers[i] - negativeTransposeModifier;
+
+        if (noteNumber < 0)
+            tooLow = true;
+
+        if (noteNumber > 24)
+            tooLow = true;
+    }
+
+    if (tooLow && !tooHigh)
+        return 12;
+
+    if (tooHigh && !tooLow)
+        return -12;
+
+    // by default, handle this case by case
+    return 0;
+}
---
+++ src/utils/transposer.test.js
@@ -0,0 +1,11 @@
+import {shiftChord} from './transposer';
+
+it('shiftChord: returns transposed chord note numbers', () => {
+
+  expect(shiftChord(0, 1, [0,4,7])).toEqual([1,5,8]);
+  expect(shiftChord(4, 3, [0,4,7])).toEqual([11,15,18]);
+  expect(shiftChord(4, 3, [1,5,8])).toEqual([0,4,7]);
+  expect(shiftChord(4, 5, [1,5,8])).toEqual([2,6,9]);
+  expect(shiftChord(4, 6, [0,11,24])).toEqual([14,13,26]);
+
+});
