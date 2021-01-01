14614081b3acbe1fa2e6d4a79530c7019ccaad35
Teran Keith <terankeith33@gmail.com>
2021-01-01 13:23:24 -0600

    adds piano object and loops to render piano html

--- src/components/PianoComponent.js
+++ src/components/PianoComponent.js
@@ -1,74 +1,20 @@
 import React from "react"
 import "../styles/Piano.css"
 import { Key } from "./Key"
+import piano from "./data/piano.json"
 
 export const PianoComponent = () => {
+  const newPiano = piano.map(pianoKey => {
+    return (
+      <li
+        key={pianoKey.note + "-" + pianoKey.ocatave}
+        className={pianoKey.color + " " + pianoKey.note}
+      ></li>
+    )
+  })
   return (
     <>
-      <ul className="set">
-        <li className="white e"></li>
-        <li className="black ds"></li>
-        <li className="white d"></li>
-        <li className="black cs"></li>
-        <li className="white c"></li>
-        <li className="white b"></li>
-        <li className="black as"></li>
-        <li className="white a"></li>
-        <li className="black gs"></li>
-        <li className="white g"></li>
-        <li className="black fs"></li>
-        <li className="white f"></li>
-        <li className="white e"></li>
-        <li className="black ds"></li>
-        <li className="white d"></li>
-        <li className="black cs"></li>
-        <li className="white c"></li>
-        <li className="white b"></li>
-        <li className="black as"></li>
-        <li className="white a"></li>
-        <li className="black gs"></li>
-        <li className="white g"></li>
-        <li className="black fs"></li>
-        <li className="white f"></li>
-        <li className="white e"></li>
-        <li className="black ds"></li>
-        <li className="white d"></li>
-        <li className="black cs"></li>
-        <li className="white c"></li>
-        <li className="white b"></li>
-        <li className="black as"></li>
-        <li className="white a"></li>
-        <li className="black gs"></li>
-        <li className="white g"></li>
-        <li className="black fs"></li>
-        <li className="white f"></li>
-      </ul>
-      {/* <div classNameName="piano-container">
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-      </div> */}
+      <ul className="set">{newPiano}</ul>
     </>
   )
 }
