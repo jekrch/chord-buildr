133083e0f9269a67738f15d03b69611d749c7107
Jacob Krch <jekrch@gmail.com>
2021-01-18 20:08:04 -0600

    an aspirational button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { PianoComponent } from "./components/PianoComponent"
 import { ChordPianoComponent } from "./components/ChordPianoComponent"
 import { ChordInput } from "./components/ChordInput"
 import { HeaderComponent } from "./components/Layout"
+import Button from "react-bootstrap/Button"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AppProvider>
         <div class="container">
           <HeaderComponent class="row" />
+          <Button variant="primary">Add Chord</Button>
           <ChordPianoComponent class="row" />
           {/* <ChordPianoComponent class="row" /> */}
         </div>
