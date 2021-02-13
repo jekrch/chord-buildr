2fbf94ddd53ee63cbbf8358067f4f83f8027c5ef
Teran Keith <terankeith33@gmail.com>
2021-02-13 11:08:13 -0600

    adds chord list to header component

--- src/components/Layout/HeaderComponent.js
+++ src/components/Layout/HeaderComponent.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React, { useContext } from "react"
 import "../../styles/Layout.css"
+import { AppContext } from "../context/AppContext"
 
 export const HeaderComponent = () => {
-  return (
-    <>
-      <header className="mainHeader">
-        <h1>Chord Buildr</h1>
-      </header>
-    </>
-  )
+	const { state } = useContext(AppContext)
+
+	const renderProgression = () => {
+		return state.chordPianoSet.map((piano, i) => {
+			return (
+				<li key={i}>
+					&nbsp;{piano.selectedChord.noteLetter}
+					{piano.selectedChord.type}&nbsp;|
+				</li>
+			)
+		})
+	}
+	return (
+		<>
+			<header className="mainHeader">
+				<h1>Chord Buildr</h1>
+				<ul className="progression" style={{ listStyle: "none" }}>
+					{renderProgression()}
+				</ul>
+			</header>
+		</>
+	)
 }
