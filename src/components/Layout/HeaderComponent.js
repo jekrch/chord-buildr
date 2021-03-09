a8de705d161fbc9878704f64737d69a59fca3274
Jacob Krch <jekrch@gmail.com>
2021-03-08 18:52:53 -0600

    About modal

--- src/components/Layout/HeaderComponent.js
+++ src/components/Layout/HeaderComponent.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
 import "../../styles/Layout.css"
 import { AppContext } from "../context/AppContext"
 import { playPiano } from "../../utils/synthPlayer"
@@ -11,6 +12,8 @@ import GitHub120 from "../../public/images/GitHub120.png"
 
 export const HeaderComponent = () => {
   const { state, dispatch } = useContext(AppContext)
+
+  const [modalShow, setModalShow] = React.useState(false)
 
   const handleClickAddChord = () => {
     dispatch({
@@ -23,6 +26,82 @@ export const HeaderComponent = () => {
     console.log("clicked " + id)
 
     playPiano(dispatch, state, id)
+  }
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="l"
+        className="aboutModal"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Chord Buildr
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Chord Buildr provides an easy way for musicians and music lovers to
+            create, share, and experiment with chord progressions.
+          </p>
+          <br />
+          <p>
+            <h5>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-music-note"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9 13c0 1.105-1.12 2-2.5 2S4 14.105 4 13s1.12-2 2.5-2 2.5.895 2.5 2z" />
+                <path fill-rule="evenodd" d="M9 3v10H8V3h1z" />
+                <path d="M8 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 13 2.22V4L8 5V2.82z" />
+              </svg>{" "}
+              tranpose
+            </h5>
+            <div className="aboutFeatureText">
+              Mark a chord as the <b>key</b> and the rest of your progression
+              will transpose as you change its root note.{" "}
+            </div>
+            <br />
+            <h5>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-music-note-beamed"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13c0-1.104 1.12-2 2.5-2s2.5.896 2.5 2zm9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2z" />
+                <path fill-rule="evenodd" d="M14 11V2h1v9h-1zM6 3v10H5V3h1z" />
+                <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4V2.905z" />
+              </svg>{" "}
+              share
+            </h5>
+            <div className="aboutFeatureText">
+              To share a progression, simply copy the URL. No account needed!
+            </div>
+            <br />
+          </p>
+          <p>
+            Chord Buildr is an{" "}
+            <a href="https://github.com/jekrch/chord-buildr">open source</a>{" "}
+            project started by{" "}
+            <a href="https://www.jacobkrch.com">Jacob Krch</a> and{" "}
+            <a href="https://www.linkedin.com/in/teran-keith-210941107/">
+              Teran Keith
+            </a>{" "}
+            using Node.Js, React, and Tone.js.
+          </p>
+        </Modal.Body>
+      </Modal>
+    )
   }
 
   const renderProgression = () => {
@@ -57,6 +136,14 @@ export const HeaderComponent = () => {
           <img className="gitSrc" src={GitHub120} />
         </Navbar.Brand>{" "}
         <div className="footNavText">v1.0</div>
+        <div className="footNavAbout" onClick={() => setModalShow(true)}>
+          {" "}
+          About
+        </div>
+        <MyVerticallyCenteredModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
       </Navbar>
 
       <Nav fixed="top" fill className="flex-column mainHeader">
