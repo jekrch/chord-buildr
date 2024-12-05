e580388a51b9ea7328c3f88ad025de6c39d32543
jekrch <jekrch@gmail.com>
2024-12-04 19:44:38 -0600

    fixed ios mute issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Route, Switch } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppProvider } from './components/context/AppContext'
@@ -7,7 +7,50 @@ import { PianoBoardComponent } from './components/PianoBoardComponent'
 import { FooterComponent } from './components/Layout/FooterComponent'
 import { ThemeProvider } from './components/theme-provider'
 
+
 const App: React.FC = () => {
+
+  // this silent audio reference is used to prevent the mute function 
+  // on iOS devices from blocking tonejs audio
+  const silentAudioRef = useRef<HTMLAudioElement>(null)
+  const audioInitializedRef = useRef(false)
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
+
+  // audio initialization
+  const initializeAudio = async () => {
+    if (!audioInitializedRef.current) {
+      try {
+        // for ios, we need to play silent audio first
+        if (isIOS) {
+          await silentAudioRef.current?.play()
+        }
+        
+        audioInitializedRef.current = true
+        
+        // cleanup listeners after successful initialization
+        document.removeEventListener('touchstart', initializeAudio)
+        document.removeEventListener('mousedown', initializeAudio)
+        document.removeEventListener('click', initializeAudio)
+      } catch (error) {
+        console.error('failed to initialize audio:', error)
+      }
+    }
+  }
+
+  useEffect(() => {
+    // event listeners for user interaction
+    document.addEventListener('touchstart', initializeAudio)
+    document.addEventListener('mousedown', initializeAudio)
+    document.addEventListener('click', initializeAudio)
+
+    // cleanup event listeners on unmount
+    return () => {
+      document.removeEventListener('touchstart', initializeAudio)
+      document.removeEventListener('mousedown', initializeAudio)
+      document.removeEventListener('click', initializeAudio)
+    }
+  }, [])
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="App">
@@ -24,6 +67,11 @@ const App: React.FC = () => {
                 />
               </Switch>
             </Router>
+            {isIOS && (
+              <audio ref={silentAudioRef} preload="auto">
+                <source src="/silence.mp3" type="audio/mp3" />
+              </audio>
+            )}
           </div>
 
           <FooterComponent />
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import './index.css'
 import './App.css'
 import './styles/Layout.css'
 import './styles/Piano.css'
-//import 'bootstrap/dist/css/bootstrap.min.css'
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const rootElement = document.getElementById('root')!
 const root = createRoot(rootElement)
